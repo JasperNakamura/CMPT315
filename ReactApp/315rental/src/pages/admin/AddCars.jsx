e88649ef779668128effb6f0ce6969b6343cf9be
Chanchatri Chaichanathong <chaichanathongc@mymacewan.ca>
2022-11-08 17:41:20 -0700

    handle input data for AddCar.jsx

--- ReactApp/315rental/src/pages/admin/AddCars.jsx
+++ ReactApp/315rental/src/pages/admin/AddCars.jsx
@@ -8,11 +8,49 @@ import Header from "../../components/AdminHeader"
 export default function AddCars () {
     const [branch, setBranch] = React.useState('');
     const [cartype, setCartype] = React.useState('');
+    const [manufacturer, setMenufacturer] = React.useState("");
+    const [model, setModel] = React.useState("");
+    const [fuelType, setFuelType] = React.useState("");
+    const [colour, setColour] = React.useState("");
+    const [licencePlate, setLicencePlate] = React.useState("");
+    const [available, setAvailable] = React.useState(true);
 
     const handleChange = (event) => {
+        console.log(event.target.id);
+        if(event.target.id === "manufacturer_input_id"){
+            setMenufacturer(event.target.value);
+        }
+        if(event.target.id === "model_input_id"){
+            setModel(event.target.value);          
+        }
+        if(event.target.id === "fueltype_input_id"){
+            setFuelType(event.target.value);        
+        }
+        if(event.target.id === "colour_input_id"){
+            setColour(event.target.value);
+        }
+        if(event.target.id === "licenceplate_input_id"){
+            setLicencePlate(event.target.value);
+        }
+        if(event.target.id === "available_input_id"){
+            setAvailable(!event.target.checked);
+        }
         setBranch(event.target.value);
         setCartype(event.target.value);
     };
+
+    const handleSubmit = (event) =>{
+        event.preventDefault();
+        const manufacturerValue = manufacturer;
+        const modelValue = model;
+        const fuelTypeValue = fuelType;
+        const colourValue = colour;
+        const availableValue = available;
+
+        console.log("Submit: ", manufacturerValue, modelValue, fuelTypeValue, colourValue, availableValue);
+    }
+
+    
 
     return (
         <div>
@@ -22,42 +60,52 @@ export default function AddCars () {
                 <h1>Add Cars to Database</h1>
                 <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
                     <div>
-                        <TextField
+                        <TextField                          
                             required
-                            id="outlined-required"
-                            label="Manufacturer"
+                            id="manufacturer_input_id"
+                            label="Manufacturer"                  
+                            value={manufacturer}
+                            onChange={handleChange}
                             /* inputRef={} */
                         />
 
                         <TextField
                             required
-                            id="outlined-required"
+                            id="model_input_id"
                             label="Model"
+                            value={model}
+                            onChange={handleChange}
                             /* inputRef={} */
                         />
 
                         <TextField
                             required
-                            id="outlined-required"
+                            id="fueltype_input_id"
                             label="FuelType"
+                            value={fuelType}
+                            onChange={handleChange}
                             /* inputRef={} */
                         />
 
                         <TextField
                             required
-                            id="outlined-required"
+                            id="colour_input_id"
                             label="Colour"
+                            value={colour}
+                            onChange={handleChange}
                             /* inputRef={} */
                         />
 
                         <TextField
                             required
-                            id="outlined-required"
+                            id="licenceplate_input_id"
                             label="LicencePlate"
+                            value={licencePlate}
+                            onChange={handleChange}
                             /* inputRef={} */
                         />
 
-                        <FormControlLabel control={<Switch/>} label="Available" labelPlacement='start' />
+                        <FormControlLabel control={<Switch id="available_input_id"/>} label="Available" labelPlacement='start' onChange={handleChange} defaultChecked={false} value={available} />
 
                         <InputLabel id="branch-simple-select-label">Branch</InputLabel>
                         <Select
@@ -79,7 +127,7 @@ export default function AddCars () {
                         >
                         </Select>
                     </div>
-                    <Button variant="contained">Submit</Button>
+                    <Button variant="contained" onClick={handleSubmit}>Submit</Button>
                 </Box>
             </Container>
 
